96626066d56d2f59e5a09da8d8d944b209372c6b
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-04-11 20:41:54 +0300

    homework done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,47 @@
+class Key {
+    private signature: number
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    public getSignature() {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    public door: boolean;
+    public tenants: Person[] = [];
+    constructor(public key: Key) {}
+
+    public comeIn(person: Person) {
+        if(this.door) {
+            this.tenants.push(person)
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true; 
+        } else {
+            this.door = false;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
